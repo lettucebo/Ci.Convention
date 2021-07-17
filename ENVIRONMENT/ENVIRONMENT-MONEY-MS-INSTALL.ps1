28cb63563d62ebd699871352366e113cf8e26c8d
Money Yu <abc12207@gmail.com>
2021-07-17 14:54:35 +0800

    move environment to Ci.Environment

--- ENVIRONMENT/ENVIRONMENT-MONEY-MS-INSTALL.ps1
+++
@@ -1,6 +0,0 @@
-## Install chocolatey package
-choco install -y streamdeck
-choco install -y obs-studio
-choco install -y powerbi --ignore-checksums
-choco install -y obs-ndi
-choco install -y zoomit
